961d2135fefba82329977eefa6d88dedcfef76c7
soni9391 <sonisree02@gmail.com>
2025-03-01 16:19:57 +0530

    add

---
+++ day-9-terraform-registry-RDS/main.tf
@@ -0,0 +1,67 @@
+module "db" {
+  source     = "github.com/CloudTechDevOps/terraform-aws-rds" #terraform registry forked into my github 
+  identifier = "demodb"
+
+  engine            = "mysql"
+  engine_version    = "5.7"
+  instance_class    = "db.t3a.large"
+  allocated_storage = 5
+  db_name  = "demodb"
+  username = "user"
+  port     = "3306"
+  iam_database_authentication_enabled = true
+  vpc_security_group_ids = ["sg-0340bacdccca3b614"]
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  # Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, in case you don't want to create it automatically
+  monitoring_interval    = "30"
+  monitoring_role_name   = "MyRDSMonitoringRole"
+  create_monitoring_role = true
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  # DB subnet group
+  create_db_subnet_group = true
+  subnet_ids             = ["subnet-0ee4fbad89fb4ad17","subnet-074c8e5d4807d9a44"]
+  # DB parameter group
+  family = "mysql5.7"
+
+  # DB option group
+  major_engine_version = "5.7"
+
+  # Database Deletion Protection
+  deletion_protection = true
+
+  parameters = [
+    {
+      name  = "character_set_client"
+      value = "utf8mb4"
+    },
+    {
+      name  = "character_set_server"
+      value = "utf8mb4"
+    }
+  ]
+
+  options = [
+    {
+      option_name = "MARIADB_AUDIT_PLUGIN"
+
+      option_settings = [
+        {
+          name  = "SERVER_AUDIT_EVENTS"
+          value = "CONNECT"
+        },
+        {
+          name  = "SERVER_AUDIT_FILE_ROTATIONS"
+          value = "37"
+        },
+      ]
+    },
+  ]
+}
