170714b8be52df50fef581885b730428f47e2388
soni9391 <sonisree02@gmail.com>
2025-03-01 20:49:53 +0530

    asdfghj

---
+++ day-10-dependent-block/main.tf
@@ -0,0 +1,18 @@
+provider "aws" { 
+    region = "ap-south-1"
+}
+
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "qwertyuiopasdfgder"
+  
+}
+
+
+
+resource "aws_instance" "dev" {
+    ami = "ami-0440d3b780d96b29d"
+    instance_type = "t2.micro"
+    depends_on = [ aws_s3_bucket.example]
+}
