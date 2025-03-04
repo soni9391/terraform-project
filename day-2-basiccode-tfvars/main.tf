f966080718dfa593c87a325e038153f79afcac10
soni9391 <sonisree02@gmail.com>
2025-03-04 12:36:17 +0530

    Update main.tf

--- day-2-basiccode-tfvars/main.tf
+++ day-2-basiccode-tfvars/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
-    ami = "ami-0d682f26195e9ec0f"
-    instance_type = "t2.micro"
-    key_name = "apple"
+    ami = var.ami_id
+    instance_type = var.type
+    key_name = var.key
   
 }
